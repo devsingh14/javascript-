c80d1f99e8ac0bb5c7fa05d3ee75a419380d3d06
devsingh14 <ssdev060@gmail.com>
2024-09-09 11:50:04 +0530

    ep01

---
+++ episode01/index.js
@@ -0,0 +1,15 @@
+//template letrals
+const age = 20;
+const fullname = "Dev Singh";
+console.log("Hello Mr " +fullname+ " your age is: "+age);
+console.log(` 
+    Hello Mr
+    ${fullname} your age is :${age}`);
+    //print sum for two numbers
+
+    const a= 14;
+    const b=6;
+    console.log("add");
+    console.log(`sum of ${a} and ${b} is = ${a+b}`);
+    
+
